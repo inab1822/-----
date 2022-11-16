6ebd4dcdca98577e170a84d4b5b862cf2e7c091c
inab1822 <inab1822@naver.com>
2022-11-16 16:05:09 +0900

    2차 수정

--- coffe.py
+++ coffe.py
@@ -45,8 +45,7 @@ class Coffee:
         print('우유 :'+str(self.resources['milk'])+'ml')
         print('커피 :'+str(self.resources['coffee'])+'g')
         print('돈 : '+str(Coffee.profit)+'원')
-    
-    
+
         
 
 
@@ -87,14 +86,37 @@ def enough(coffee_name):
 def put_ur_money(coffee_name):
     ur_coffee_m = Menu[coffee_name]['cost']
     print(f'{ur_coffee_m:,} 입니다. 돈을 넣어주세요')
-    # man = 
-    # ochun = 
-    # chun = 
-    # obec = 
-    # bec = 
-    # oship = 
-    # ship = 
+    man = int(input('만원'))
+    ochun = int(input('오천원'))
+    chun = int(input('천원'))
+    obec = int(input('오백원'))
+    bec = int(input('백원'))
+    oship = int(input('오십원'))
+    ship = int(input('십원'))
+    inserted_coin = man * 10000 + ochun * 5000 + chun * 1000 + obec * 500 + bec * 100 + oship * 50 + ship * 10
+    if inserted_coin >= ur_coffee_m:
+        ur_charge = inserted_coin - ur_coffee_m
+        print(f'넣으신 금액은{inserted_coin:,}원 입니다.')
+        print(f'거스름돈 {ur_charge:,}원 받아가세요.')
+        coffee_maker(coffee_name, ur_coffee_m)
+      
+    else:
+        print(f'넣으신 금액은{inserted_coin:,}원 입니다.')
+        print('금액이 모자랍니다. 다시 돈을 넣어주세요')
+        put_ur_money(coffee_name)
+  
     
-    
+def coffee_maker(coffee_name,ur_coffee_m):
+    print(f'{coffee_name}을(를) 만들고 있습니다. 잠시만 기다려 주세요.')
+    coffee_name_stuff = Menu[coffee_name]['ingredients']
+    user_w= coffee_name_stuff['water']
+    user_m= coffee_name_stuff['milk']
+    user_c= coffee_name_stuff['coffee']
+    re = Coffee(Menu,resources)
+    re.resources['water'] -= user_w
+    re.resources['milk'] -= user_m
+    re.resources['coffee'] -= user_c
+    Coffee.profit += ur_coffee_m
   
 coffee_prompt()
+
